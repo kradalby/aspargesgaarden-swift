89007895f25b7f4417e56dec64f929a8b4133c10
Kristoffer Dalby <kradalby@kradalby.no>
2020-09-03 21:56:57 +0100

    Run prettier

--- meny.js
+++ meny.js
@@ -1,9 +1,9 @@
 const navSlide = () => {
-  const hamburger = document.querySelector('.hamburger');
-  const nav = document.querySelector('.nav-links');
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav-links");
 
-  hamburger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
   });
 };
 
