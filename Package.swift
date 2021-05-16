d8fc55a28b0943d44d3dc4bef4ce5b1ef4b37883
Kristoffer Dalby <kradalby@kradalby.no>
2021-05-16 16:42:08 +0100

    Swift lint

--- Package.swift
+++ Package.swift
@@ -3,20 +3,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "Aspargesgaarden",
-    products: [
-        .executable(
-            name: "Aspargesgaarden",
-            targets: ["Aspargesgaarden"]
-        )
-    ],
-    dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.6.0")
-    ],
-    targets: [
-        .target(
-            name: "Aspargesgaarden",
-            dependencies: ["Publish"]
-        )
-    ]
+  name: "Aspargesgaarden",
+  products: [
+    .executable(
+      name: "Aspargesgaarden",
+      targets: ["Aspargesgaarden"]
+    )
+  ],
+  dependencies: [
+    .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.6.0")
+  ],
+  targets: [
+    .target(
+      name: "Aspargesgaarden",
+      dependencies: ["Publish"]
+    )
+  ]
 )
